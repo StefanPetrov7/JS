aa760062bcd439b3ed3a69c7123dc130b91bbe39
Stefan Petrov <76209920+StefanPetrov7@users.noreply.github.com>
2021-05-23 23:36:13 +0300

    Delete JS_Advanced/DOM_Exercise/03. Accordion directory

--- JS_Advanced/DOM_Exercise/03. Accordion/accordion.js
+++
@@ -1,7 +0,0 @@
-function toggle() {
-
-    let button = document.querySelector('.button');
-    let divExtra = document.querySelector('#extra');
-    divExtra.style.display === 'block' ? divExtra.style.display = 'none' : divExtra.style.display = 'block';
-    button.textContent === 'More' ? button.textContent = 'Less' : button.textContent = 'More';
-}
